a787abb35a502a2f1fea5e560a2345f68551b553
Dremafia <137164652+Dremafia@users.noreply.github.com>
2023-10-11 11:56:35 +0100

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -24,5 +24,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' Day(s) ️', h, ' Hour(s) ', m, ' Minute(s)'].map(v => v.toString().padStart(2, 0)).join('')
+  return ['they don't really fuck with the one's like us'].map(v => v.toString().padStart(2, 0)).join('')
 }
