3b2dd25db5b30d70e4ed51ba191eb1d4150aee08
Leagold7 <148663234+Leagold7@users.noreply.github.com>
2023-10-22 02:54:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['2349151610726', 'Dark-side', true],
-  ['2349021121564'], 'spaghetti-mafia', true]
+  ['237620192647', 'Dark-side', true],
+  ['237620192647'], 'Freeman', true]
   [''] 
 ] //Number of owners
 
-global.mods = ['2349151610726','2349021121564'] 
-global.prems = ['2349021121564', '2349151610726', '2349151610726']
+global.mods = ['237620192647','237620192647'] 
+global.prems = ['237620192647', '237620192647', '237620192647']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
